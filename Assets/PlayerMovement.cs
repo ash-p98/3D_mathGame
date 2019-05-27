84cfc11718b74396196387554a075f5205628575
ash-p98 <32458304+ash-p98@users.noreply.github.com>
2019-05-27 14:27:53 +0100

    Add files via upload

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed; //move speed is float can be changed in unity
+    private Vector3 input;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rigidbody.AddForce(input * moveSpeed);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# 3D_mathgame
+# Math-Game
